0ed934747cb5f81e754f9382d32052f58874d498
KrishRathi <krishrathi877@gmail.com>
2025-12-13 12:38:21 +0530

    refactor: improve switch component

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -5,6 +5,7 @@ import * as SwitchPrimitives from "@radix-ui/react-switch"
 
 import { cn } from "@/lib/utils"
 
+// Switch component with improved toggle functionality
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
   React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
