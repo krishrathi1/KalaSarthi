7e36b993754fc57a19b1e97b3205ecbae5614b8f
KrishRathi <krishrathi877@gmail.com>
2025-12-13 12:37:23 +0530

    refactor: improve UI components with better comments

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+// Card component with enhanced styling
 const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
