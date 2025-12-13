e6e1c6b761f6d3d5a619f8013f09af802f75f92c
KrishRathi <krishrathi877@gmail.com>
2025-12-13 12:37:27 +0530

    refactor: enhance badge and avatar components

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -5,6 +5,7 @@ import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
 import { cn } from "@/lib/utils"
 
+// Avatar component with improved image handling
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
