34a415d7d9956aa1ce77eed133191b785353ce4d
KrishRathi <krishrathi877@gmail.com>
2025-12-14 18:15:11 +0530

    feat: enhance badge and avatar components with accessibility improvements

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -24,10 +24,11 @@ Avatar.displayName = AvatarPrimitive.Root.displayName
 const AvatarImage = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Image>,
   React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Image>
->(({ className, ...props }, ref) => (
+>(({ className, onError, ...props }, ref) => (
   <AvatarPrimitive.Image
     ref={ref}
-    className={cn("aspect-square h-full w-full", className)}
+    className={cn("aspect-square h-full w-full object-cover", className)}
+    onError={onError}
     {...props}
   />
 ))
